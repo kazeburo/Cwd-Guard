c033340a3110b5901842d925606f9a3acb0ef5c4
Masahiro Nagano <kazeburo@gmail.com>
2011-01-27 16:31:54 +0900

    initial commit

---
+++ lib/Cwd/Guard.pm
@@ -0,0 +1,32 @@
+package Cwd::Guard;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Cwd::Guard -
+
+=head1 SYNOPSIS
+
+  use Cwd::Guard;
+
+=head1 DESCRIPTION
+
+Cwd::Guard is
+
+=head1 AUTHOR
+
+Masahiro Nagano E<lt>kazeburo {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
