74333fbd119e9492a31c1349d2715655e8ef6b72
Masahiro Nagano <kazeburo@gmail.com>
2011-01-27 17:11:15 +0900

    first commit

--- lib/Cwd/Guard.pm
+++ lib/Cwd/Guard.pm
@@ -1,22 +1,70 @@
 package Cwd::Guard;
+
 use strict;
 use warnings;
+use Cwd qw/getcwd/;
+use autodie qw/chdir/;
+use parent 'Exporter';
+
+our @EXPORT_OK = qw/cwd_guard/;
+
 our $VERSION = '0.01';
+
+sub cwd_guard {
+    my $dir = shift;
+    __PACKAGE__->new($dir);
+}
+
+sub new {
+    my $class = shift;
+    my $dir = shift;
+    my $cwd = getcwd();
+    my $callback = sub {
+        chdir $cwd;
+    };
+    defined $dir ? chdir($dir) : chdir();
+    bless \$callback, $class;
+}
+
+sub DESTROY {
+    ${$_[0]}->();
+}
+
 
 1;
 __END__
 
 =head1 NAME
 
-Cwd::Guard -
+Cwd::Guard - Temporary changing working directory
 
 =head1 SYNOPSIS
 
-  use Cwd::Guard;
+  use Cwd::Guard qw/cwd_guard/;
+  use Cwd;
+
+  my $dir = getcwd;
+  MYBLOCK: {
+      my $guard = cwd_guard('/tmp/xxxxx');
+      # chdir to /tmp/xxxxx
+  }
+  # back to $dir
+
 
 =head1 DESCRIPTION
 
-Cwd::Guard is
+CORE::chdir Cwd:: Guard can change the current directory (chdir) using a limited scope.
+
+=head1 FUNCTIONS
+
+=over 4
+
+=item cwd_guard($dir);
+
+chdir to $dir and returns Cwd::Guard object. return to current working directory, if this object destroyed.
+if failed to chdir, Cwd::Guard die immediately. 
+
+=back
 
 =head1 AUTHOR
 
--- t/00_compile.t
+++ t/00_compile.t
@@ -2,3 +2,4 @@ use strict;
 use Test::More tests => 1;
 
 BEGIN { use_ok 'Cwd::Guard' }
+
